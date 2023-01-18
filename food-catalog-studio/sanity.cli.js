05df9d828bb3f75f0e189e609df89d1f35d9d082
Duha Wajeeh <duha_hendi@startappz.com>
2023-01-18 22:14:34 +0300

    initial commit

---
+++ food-catalog-studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'mn7wwdie',
+    dataset: 'production'
+  }
+})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";// views
+import Home from "./views/Home";
+import About from "./views/About";
+import Foods from "./views/Foods";
+import FilteredFoods from "./views/FilteredFoods";
+// components
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+
+// styles
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* Header Area */}
+      <div className="max-w-full">
+        <Header />
+      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/About" element={<About />} />
+        </Routes>
+        <Routes>
+          <Route path="/Foods" element={<Foods />} />
+        </Routes>
+        <Routes>
+          <Route path="/filteredfoods/:slug" element={<FilteredFoods />} />
+        </Routes>
+      {/* Footer Area */}
+			<div className="max-w-full">
+				<Footer />
+			</div>
+    </BrowserRouter>
   );
 }
 
